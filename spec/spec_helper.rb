0836e14f1f663bc17f91f631ba256c35d3eacdf3
Janosch Müller <janosch.mueller@betterplace.org>
2022-05-09 23:19:18 +0200

    Fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "repper"
 require_relative "support/matchers"
 
+Rainbow.enabled = true # force colors even on non-tty / dumb terminal
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
